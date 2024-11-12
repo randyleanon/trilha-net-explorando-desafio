efe50fe566c54e9fcd966d8574ecc9f0dad8165c
randyleanon <jorgelucasdlcruz@gmail.com>
2024-11-12 17:10:36 -0300

    Add files via upload
    Upload do arquivo "Reserva.cs"

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,16 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTE AQUI* - CHECK
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTE AQUI* - CHECK
+                throw new Exception("Quantidade de hóspedes excede o máximo definido"); //Excepção especifica
             }
         }
 
@@ -36,22 +37,22 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *IMPLEMENTE AQUI* - CHECK
+            return Hospedes.Count; //Autoexplicativo
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // *IMPLEMENTE AQUI* - CHECK
+            decimal valor = DiasReservados * Suite.ValorDiaria; //Autoexplicativo
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTE AQUI* - CHECK
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9m; //valor = valor * 0.9m (10% de desconto)
             }
 
             return valor;
